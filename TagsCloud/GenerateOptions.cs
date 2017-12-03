39b72f6f531cbf166135f4935d0811b905554461
Leofwin <andy_namor@mail.ru>
2017-12-03 12:38:17 +0500

    add pasring console attributes

---
+++ TagsCloud/GenerateOptions.cs
@@ -0,0 +1,42 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace TagsCloud
+{
+	public class GenerateOptions
+	{
+		[Option('w', "width", Required = true,
+			HelpText = "Width of output file.")]
+		public int Width { get; set; }
+
+		[Option('h', "height", Required = true,
+			HelpText = "Height of output file.")]
+		public int Height { get; set; }
+
+		[Option('i', "input", Required = true,
+			HelpText = "Name of input file")]
+		public string InputFileName { get; set; }
+
+		[Option('o', "output", Required = true,
+			HelpText = "Name of output file")]
+		public string OutputFileName { get; set; }
+
+		[Option('c', "color", DefaultValue = "Blue",
+			HelpText = "Color of text in output file")]
+		public string ColorName { get; set; }
+
+		[Option('l', "limit", DefaultValue = 100,
+			HelpText = "Words count")]
+		public int WordsCount { get; set; }
+
+		[ParserState]
+		public IParserState LastParserState { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this,
+				current => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
--- TagsCloud/Program.cs
+++ TagsCloud/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
 using Autofac;
-
+using CommandLine;
 
 namespace TagsCloud
 {
@@ -9,7 +9,12 @@ namespace TagsCloud
 	{
 		public static void Main(string[] args)
 		{
-			var size = new Size(1000, 1000);
+			args = new[] { "-h", "1000", "-w", "1000", "-i", "../../texts/Master.txt", "-o", "result.png"};
+			var options = new GenerateOptions();
+			if (!Parser.Default.ParseArguments(args, options))
+				return;
+
+			var size = new Size(options.Width, options.Height);
 			var fontFamily = FontFamily.GenericMonospace;
 
 			var builder = new ContainerBuilder();
@@ -18,16 +23,18 @@ namespace TagsCloud
 			builder.RegisterInstance(new WordValidator()).As<IWordValidator>();
 			builder.RegisterType<WordFrequencySaver>().As<IWordFrequencySaver>();
 			builder.RegisterInstance(new FontNormalizer(10, 60)).As<IFontNormalizer>();
-			builder.RegisterInstance(new AcrhimedeCircularCloudLayouter(new Point(500, 500)))
+			builder.RegisterInstance(new AcrhimedeCircularCloudLayouter(
+					new Point(options.Width / 2, options.Height / 2))
+				)
 				.As<ICircularCloudLayouter>();
 			builder.RegisterInstance(new SizeDetector()).As<ISizeDetector>();
 			builder.RegisterType<CloudMaker>();
 
 			var containter = builder.Build();
 			var cloudMaker = containter.Resolve<CloudMaker>();
-			var bitmap = cloudMaker.GenerateImage("../../texts/Sherlock_Holmes.txt", 
-				size, Color.Blue, fontFamily, 400);
-			bitmap.Save("result.png", ImageFormat.Png);
+			var bitmap = cloudMaker.GenerateImage(options.InputFileName,
+				size, Color.FromName(options.ColorName), fontFamily, options.WordsCount);
+			bitmap.Save(options.OutputFileName, ImageFormat.Png);
 		}
 	}
 }
